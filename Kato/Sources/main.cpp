fb73941d54027117c446f68e539793e2c4470a23
WurzelPeter <yunhyeokchoi.digipen@gmail.com>
2017-07-24 16:00:46 +0900

    Implemented a simple game loop responding for the key inputs. KatoEngine class have only Update function visible.

--- Kato/Sources/main.cpp
+++ Kato/Sources/main.cpp
@@ -1,30 +1,74 @@
 #include <SDL.h>
+//For printf, since we don't have any debug tools which can break the program atm.
 #include <stdio.h>
+//This is where all the pointers to image files will be stored.
+#include <list>
 
-class SDLCls
+class KatoEngine
 {
 	public:
-		SDLCls(const char* title, 
-				 unsigned int scrwidth, unsigned int scrheight)
+		KatoEngine(const char* title,
+					  unsigned int scrwidth, unsigned int scrheight)
 			: pm_scrwidth(scrwidth), pm_scrheight(scrheight),
-			  pm_title(title)
+			  pm_title(title), pm_isquit(false)
 		{
 			SDLInit();
 		}
 
-		void Quit()
+		void Update()
 		{
-			SDL_DestroyWindow(pm_psdlwin);
-			SDL_Quit();
+			SDL_Event eventhnd; //an event handler
 
-			SDL_DestroyWindow(pm_psdlwin);
-			SDL_Quit();
+			while (!pm_isquit)
+			{
+				//Till the event gets emptied out.
+				while (SDL_PollEvent(&eventhnd))
+				{
+					/*
+						https://wiki.libsdl.org/SDL_EventType#Remarks
+						#1 An SDL_QUIT event is generated when the user clicks 
+							on the close button of the last existing window.
+					*/
+					if (eventhnd.type == SDL_QUIT)
+					{
+						pm_isquit = true;
+						//Stop assessing for the leftover msgs, and just break.
+						//break;
+					}
+					else if (eventhnd.type == SDL_KEYDOWN)
+					{
+						switch (eventhnd.key.keysym.sym)
+						{
+							case SDLK_UP :
+							{
+								printf("UP\n");
+								break;
+							}
+							case SDLK_DOWN :
+							{
+								printf("DOWN\n");
+								break;
+							}
+							case SDLK_LEFT :
+							{
+								printf("LEFT\n");
+								break;
+							}
+							case SDLK_RIGHT :
+							{
+								printf("RIGHT\n");
+								break;
+							}
+						}
+					}
+					
+				}
+			}
 
-			SDL_DestroyWindow(pm_psdlwin);
-			SDL_Quit();
+			this->Quit();
 		}
 
-		~SDLCls() {};
+		~KatoEngine() {};
 
 	private:
 		void SDLInit()
@@ -51,9 +95,15 @@ class SDLCls
 					SDL_FillRect(pm_psurf, 0, SDL_MapRGB(pm_psurf->format, 0xFF, 0xFF, 0xFF));
 					//SwapBuffer(?)
 					SDL_UpdateWindowSurface(pm_psdlwin);
-					SDL_Delay(5000);
+					//SDL_Delay(5000);
 				}
 			}
+		}
+
+		void Quit()
+		{
+			SDL_DestroyWindow(pm_psdlwin);
+			SDL_Quit();
 		}
 
 		unsigned int pm_scrwidth;
@@ -62,12 +112,17 @@ class SDLCls
 
 		SDL_Window* pm_psdlwin;
 		SDL_Surface* pm_psurf;
+
+		bool pm_isquit;
+		std::list<SDL_Surface*> pm_listpimage;
 };
 
+//Function signature for the main should be like this,
+//so as to make it compatible with SDL.
 int main(int, char*[])
 {
-	SDLCls sdlcls("Supreme Lord Yunhyeok", 800, 600);
-	sdlcls.Quit();
+	KatoEngine engine("Supreme Lord Yunhyeok", 800, 600);
+	engine.Update();
 
 	return 0;
 }
